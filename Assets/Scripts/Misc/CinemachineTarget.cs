b0021a9452a2bf26af9fd6e100b1ce057245d93c
ChuDucAnh242002 <anh.chu@tuni.fi>
2022-08-20 08:26:20 +0300

    create door prefab

--- Assets/Scripts/Misc/CinemachineTarget.cs
+++ Assets/Scripts/Misc/CinemachineTarget.cs
@@ -9,6 +9,8 @@ public class CinemachineTarget : MonoBehaviour
 {
     private CinemachineTargetGroup cinemachineTargetGroup;
 
+    [SerializeField] private Transform cursorTarget;
+
     private void Awake(){
         cinemachineTargetGroup = GetComponent<CinemachineTargetGroup>();
     }
@@ -20,14 +22,26 @@ public class CinemachineTarget : MonoBehaviour
     private void SetCinemachineTargetGroup(){
         CinemachineTargetGroup.Target cinemachineGroupTarget_player = new CinemachineTargetGroup.Target {
             weight = 1f,
-            radius = 1f,
+            radius = 2.5f,
             target = GameManager.Instance.GetPlayer().transform
         };
 
-        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[] {
-            cinemachineGroupTarget_player
+        CinemachineTargetGroup.Target cinemachineGroupTarget_cursor = new CinemachineTargetGroup.Target {
+            weight = 0.3f,
+            radius = 1f,
+            target = cursorTarget
+        };
+
+        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[]{
+            cinemachineGroupTarget_player,
+            cinemachineGroupTarget_cursor
         };
 
         cinemachineTargetGroup.m_Targets = cinemachineTargetArray;
+    }
+
+    private void Update(){
+        cursorTarget.position = HelperUtilities.GetMouseWorldPosition();
+
     }
 }
