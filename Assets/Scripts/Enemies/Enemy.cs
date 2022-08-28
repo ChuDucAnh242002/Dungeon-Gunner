dea56dbd20adcead450e7fcbf9caa0ce95cb006e
ChuDucAnh242002 <anh.chu@tuni.fi>
2022-08-28 15:50:24 +0300

    Enemy prefab

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -3,7 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+#region REQUIRE COMPONENTS
+[RequireComponent(typeof(SortingGroup))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+
+#endregion REQUIRE COMPONENTS
+[DisallowMultipleComponent]
 public class Enemy : MonoBehaviour
 {
+    [HideInInspector] public EnemyDetailsSO enemyDetails;
+    [HideInInspector] public SpriteRenderer[] spriteRendererArray;
 
+    private CircleCollider2D circleCollider2D;
+    private PolygonCollider2D polygonCollider2D;
+
+    private void Awake(){
+        circleCollider2D = GetComponent<CircleCollider2D>();
+        polygonCollider2D = GetComponent<PolygonCollider2D>();
+        spriteRendererArray = GetComponentsInChildren<SpriteRenderer>();
+    }
 }
